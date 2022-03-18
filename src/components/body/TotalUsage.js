a232237812357358b789d5f2624f25ceb9f26e24
Rubel Hossain <rubrex96@hotmail.com>
2022-03-18 22:38:32 +0600

    [styles] to TotalUsage Component

--- src/components/body/TotalUsage.js
+++ src/components/body/TotalUsage.js
@@ -8,25 +8,32 @@ class TotalUsage extends Component {
             data: stateJSON
         }
         const openwrtUser = this.state.data.data.map(user => {
-            console.log(user)
+            console.log(user);
+            const idle = man => true;
             return (
 
-                <div key={user.user}>
+                <div key={user.user} className="totalusage_container">
                     <h3>{user.user}</h3>
-                    <p>Download : {(Number(user.download) / 1024).toFixed(2)}MB</p>
-                    <p>Upload : {(Number(user.upload) / 1024).toFixed(2)}MB</p>
-                    <p>Total Download : {(Number(user.totaldownload) / 1024).toFixed(2)}MB</p>
-                    <p>Total Upload : {(Number(user.totalupload) / 1024).toFixed(2)}MB</p>
+                    <div className='totalusage_stats'>
+                        <p>Download : {(Number(user.download) / 1024).toFixed(2)}MB</p>
+                        <p>Upload : {(Number(user.upload) / 1024).toFixed(2)}MB</p>
+                        <p>Total Download : {(Number(user.totaldownload) / 1024).toFixed(2)}MB</p>
+                        <p>Total Upload : {(Number(user.totalupload) / 1024).toFixed(2)}MB</p>
+                    </div>
+
                 </div>
 
             )
         });
         return (
 
-            <div>
+            <div >
                 <h1>TotalUsage</h1>
                 <hr />
-                {openwrtUser}
+                <div className='totalUsage_parent'>
+                    {openwrtUser}
+                </div>
+
             </div >
         );
     }
