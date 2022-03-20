c2140f7f52c939006af8b8e8013351fdd5dcec16
Rubel Hossain <rubrex96@hotmail.com>
2022-03-21 03:20:12 +0600

    [Added] Speed component

--- src/components/body/Speed.js
+++ src/components/body/Speed.js
@@ -1,12 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import timeLapseState from './speedState.json'
 
-function Speed() {
-    return (
-        <div>
-            <h1>Speed</h1>
-            <hr />
-        </div>
-    );
+class Speed extends Component {
+    render() {
+        this.state = {
+            timeLapseState
+        }
+        ChartJS.register(
+            CategoryScale,
+            LinearScale,
+            PointElement,
+            LineElement,
+            Title,
+            Tooltip,
+            Legend
+        );
+        const options = {
+            padding: true,
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Router Model: Mi4C',
+                },
+            },
+        };
+        const options1 = {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Router Model: Mi4A',
+                },
+            },
+        };
+        const calculateData = data => parseFloat(Number(data) / 1024);
+        // console.log(calculateData());
+        // label = Timeline
+        const labels = timeLapseState.router1.map(timeState => timeState.time);
+        // Data Array for download
+        const downloadArray = timeLapseState.router1.map(downState => calculateData(downState.download))
+        // Data array for upload
+        const uploadArray = timeLapseState.router1.map(upState => calculateData(upState.upload))
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Download',
+                    data: downloadArray.map(me => me),
+                    fill: false,
+                    lineTension: 0.4,
+                    backgroundColor: '#151D3B',
+                    borderColor: '#00C897',
+                    borderWidth: 2,
+                    pointRadius: 6,
+                    pointBorderWidth: 4,
+                    pointHoverRadius: 8,
+                },
+                {
+                    label: 'Upload',
+                    data: uploadArray,
+                    fill: false,
+                    lineTension: 0.4,
+                    backgroundColor: '#151D3B',
+                    borderColor: '#FFC300',
+                    borderWidth: 2,
+                },
+            ],
+        };
+        return (
+            <div>
+                <h2 style={{ marginTop: '10px', fontWeight: '300' }}>Speed</h2>
+                <hr />
+                <div className="canvus-container">
+                    <div className="router1">
+                        <Line
+                            width={document.documentElement.clientWidth}
+                            height={document.documentElement.clientHeight / 3}
+                            options={options}
+                            data={data}
+
+                        />
+                    </div>
+                    <div className="router2">
+                        <Line
+                            width={document.documentElement.clientWidth}
+                            height={document.documentElement.clientHeight / 3}
+                            options={options1}
+                            data={data}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default Speed;
--- src/components/body/TotalUsage.js
+++ src/components/body/TotalUsage.js
@@ -8,7 +8,6 @@ class TotalUsage extends Component {
             data: stateJSON
         }
         const openwrtUser = this.state.data.data.map(user => {
-            console.log(user);
             const calculateData = data => (Number(data) / 1024) < 1024 ? (Number(data) / 1024).toFixed(2) + " MB" : ((Number(data) / 1024) / 1024).toFixed(2) + " GB";
             const styles = { indicator_icon: { width: '40px' } }
             return (
@@ -16,17 +15,13 @@ class TotalUsage extends Component {
                 <div key={user.user} className="totalusage_container">
                     <h4 >{user.user}</h4>
                     <div className='totalusage_stats'>
-                        {/* <p>Download : {calculateData(user.download)}</p>
-                        <p>Upload : {calculateData(user.upload)}</p> */}
                         <p>
-
                             <img style={styles.indicator_icon} src="./assets/icons/cloud-download.png" alt="download_icon" />
                             <strong>
                                 {calculateData(user.totaldownload)}
                             </strong>
                         </p>
                         <p>
-
                             <img style={styles.indicator_icon} src="./assets/icons/cloud-upload.png" alt="download_icon" />
                             <strong>
                                 {calculateData(user.totalupload)}
@@ -41,7 +36,7 @@ class TotalUsage extends Component {
         return (
 
             <div >
-                <h1 style={{ marginTop: '10px' }}> Data Usage of Clients</h1>
+                <h2 style={{ marginTop: '10px', fontWeight: '300' }}> Data Usage of Clients</h2>
                 <hr />
                 <div className='totalUsage_parent'>
                     {openwrtUser}
