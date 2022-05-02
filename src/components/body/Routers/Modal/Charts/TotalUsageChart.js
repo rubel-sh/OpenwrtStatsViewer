4564df0b24815967b16109507b38546f2a89d181
Rubel Hossain <rubrex96@hotmail.com>
2022-05-03 03:17:46 +0600

    Pie Color changes

--- src/components/body/Routers/Modal/Charts/TotalUsageChart.js
+++ src/components/body/Routers/Modal/Charts/TotalUsageChart.js
@@ -100,20 +100,12 @@ const Chart = (props) => {
                 label: 'Donloads in MB',
                 data: [tDownloadClient, tUploadClient],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
+                    "#069A8E",
+                    "#A1E3D8"
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
+                    "#069A8E",
+                    "#A1E3D8"
                 ],
                 borderWidth: 1,
             },
@@ -140,6 +132,7 @@ const Chart = (props) => {
             justifyContent: 'space-around',
             alignItems: 'center',
             width: '100%',
+
         },
         p: {
             marginTop: '10px',
@@ -157,7 +150,8 @@ const Chart = (props) => {
             display: 'flex',
             flexBasis: '20%',
             justifyContent: 'space-around',
-            alignItems: 'center'
+            alignItems: 'center',
+            marginBottom: '15px'
         },
         downUpText: {
             display: 'flex',
