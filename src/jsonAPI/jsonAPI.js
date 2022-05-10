e6e9bed868b0a68a9f598db050a64825f26b833d
Rubel Hossain <rubrex96@hotmail.com>
2022-05-11 02:13:29 +0600

    API changed

--- src/jsonAPI/jsonAPI.js
+++ src/jsonAPI/jsonAPI.js
@@ -3,11 +3,11 @@ export const WHOLE_USAGE_API = "https://node.rexopenwrt.repl.co/output/counter";
 // export const WHOLE_USAGE_API = "https://node.diwash5.repl.co/output/counter";
 
 // ActionCreator.js Line 41
-// export const GRAPHDATA_API = "https://node.rexopenwrt.repl.co/output/graphdata";
-export const GRAPHDATA_API = "https://node.diwash5.repl.co/output/graphdata";
+export const GRAPHDATA_API = "https://node.rexopenwrt.repl.co/output/graphdata";
+// export const GRAPHDATA_API = "https://node.diwash5.repl.co/output/graphdata";
 
 // Only Get Request
-// export const CLIENT_LIST_API = "https://node.diwash5.repl.co/temp";
-// export const ROUTER_STATS_API = "https://node.diwash5.repl.co/temp";
 export const CLIENT_LIST_API = "https://node.rexopenwrt.repl.co/temp";
 export const ROUTER_STATS_API = "https://node.rexopenwrt.repl.co/temp";
+// export const CLIENT_LIST_API = "https://node.diwash5.repl.co/temp";
+// export const ROUTER_STATS_API = "https://node.diwash5.repl.co/temp";
